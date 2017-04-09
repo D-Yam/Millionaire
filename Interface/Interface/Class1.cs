3fff7d87467347df15f12a00df3f6b398d667d5d
RY <lolexmi-microsoft@yahoo.co.jp>
2017-04-10 01:47:24 +0900

    Update Interface

--- Interface/Interface/Class1.cs
+++ Interface/Interface/Class1.cs
@@ -7,37 +7,41 @@ namespace Interface
 	[ServiceContract]
 	public interface IWcfInterface
 	{
-		[OperationContract]
 		
 		/// <summary>
 		/// 今の自分のステータスを取得します。
 		/// </summary>
 		/// <param name="id">自分のID</param>
 		/// <returns>ステータス</returns>
+		[OperationContract]
 		Status GetMyStatus(string id);
 
 		/// <summary>
 		/// 現在一番上に出されているカード
 		/// </summary>
 		/// <returns>カードのリスト</returns>
+		[OperationContract]
 		List<Card> Layout();
 
 		/// <summary>
 		/// 最後に場札が流れたあと最初に出されたカード
 		/// </summary>
 		/// <returns>カードのリスト</returns>
+		[OperationContract]
 		List<Card> Lead();
 
 		/// <summary>
 		/// 最後に場札が流れてから今までに出されたカード
 		/// </summary>
 		/// <returns>indexが若いほど古いカードです。</returns>
+		[OperationContract]
 		List<List<Card>> TableCards();
 
 		/// <summary>
 		/// 今までに流れたカードのリスト
 		/// </summary>
 		/// <returns>indexが若いほど古いカードです。</returns>
+		[OperationContract]
 		List<List<Card>> Discards();
 
 
